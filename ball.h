7362a81947423528cf8ed41c0efb97213ae0496d
whyteferg <lexusbeat@gmail.com>
2012-10-14 21:24:24 +1300

    ball changes

--- ball.h
+++ ball.h
@@ -8,11 +8,15 @@
 #include "Collidable.h"
 class ball: collidable{
 public:
-
+	float radi;
 	ball(float radius, G308_Point initialposition, G308_Point initialvelocity);
 	void render();
 	void updateTic();
 	void applyForce(G308_Point*);
+	bool willcollidenormal(G308_Point* normal);
+	bool collideNormal(G308_Point* normal);
+	void applyTickMovement();
+	bool ballwillcollide(collidable* otherball);
 	bool collision(collidable*);
 	 G308_Point* getPosition();
 	 G308_Point* getVector();
